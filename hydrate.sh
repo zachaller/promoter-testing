6e22ccec49bc0b7ab61759dbbe31cfe0471a2bf4
Zach Aller <zach_aller@intuit.com>
2024-04-17 11:42:59 -0500

    hydrate script
    Signed-off-by: Zach Aller <zach_aller@intuit.com>

---
+++ hydrate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+git checkout main
+git pull
+
+helm template . > manifest-next.yaml
+git add values.yaml
+git commit -s -S  -m "hydrate script"
+git push
+echo "Pushed to dry branch"
+
+DRYSHA=`git rev-parse main`
+echo $DRYSHA
+
+git checkout environment/development-next
+git pull
+
+rm manifest.yaml
+mv manifest-next.yaml manifest.yaml
+git add manifest.yaml
+git commit -s -S  -m "hydration of $DRYSHA"
+git push
+echo "Pushed to hydrated branch"
+
+
+git checkout main
