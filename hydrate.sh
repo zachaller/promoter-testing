2746f16b7c50f0fa5b2a8562d8a32a09964f3e55
Zach Aller <zach_aller@intuit.com>
2024-05-22 13:44:06 -0700

    add hydrate
    Signed-off-by: Zach Aller <zach_aller@intuit.com>

--- hydrate.sh
+++ hydrate.sh
@@ -32,3 +32,75 @@ echo "Pushed to hydrated branch"
 
 
 git checkout main
+
+# Staging VVVVVV
+
+git checkout main
+git pull
+
+helm template . > manifest-next.yaml
+git add values.yaml
+git commit -s -S  -m "hydrate script"
+git push
+echo "Pushed to dry branch"
+
+DRYSHA=`git rev-parse main`
+echo $DRYSHA
+
+git checkout environment/staging-next
+git pull
+
+cat << EOF > hydrator.metadata
+{
+  "commands": ["hydrate.sh"],
+  "drySHA": "$DRYSHA"
+}
+EOF
+
+rm manifest.yaml
+mv manifest-next.yaml manifest.yaml
+git add manifest.yaml
+git add hydrator.metadata
+git commit -s -S  -m "hydration of $DRYSHA"
+git push
+echo "Pushed to hydrated branch"
+
+
+git checkout main
+
+
+# Production VVVVVVVVV
+
+
+git checkout main
+git pull
+
+helm template . > manifest-next.yaml
+git add values.yaml
+git commit -s -S  -m "hydrate script"
+git push
+echo "Pushed to dry branch"
+
+DRYSHA=`git rev-parse main`
+echo $DRYSHA
+
+git checkout environment/production-next
+git pull
+
+cat << EOF > hydrator.metadata
+{
+  "commands": ["hydrate.sh"],
+  "drySHA": "$DRYSHA"
+}
+EOF
+
+rm manifest.yaml
+mv manifest-next.yaml manifest.yaml
+git add manifest.yaml
+git add hydrator.metadata
+git commit -s -S  -m "hydration of $DRYSHA"
+git push
+echo "Pushed to hydrated branch"
+
+
+git checkout main
